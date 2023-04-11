2322b2d1671f756a72ebb97a13fa25f9f13e2727
Autumn Han <autumn9894@gmail.com>
2023-04-10 21:35:24 -0500

    can now store multiple accounts on one user

--- practice/user_bank_account.py
+++ practice/user_bank_account.py
@@ -1,22 +1,22 @@
 class BankAccount:
     bank_name = "Coding Dojo Credit Union"
-    def __init__(self, name, balance, int_rate):
-        self.name = name
+    def __init__(self, account_name, balance, int_rate):
+        self.account_name = account_name
         self.balance = balance
         self.int_rate = int_rate
-    def deposit(self, amount): # increases the account balance by the given amount
+    def deposit(self, amount): 
         self.balance = self.balance + amount
         return self
-    def withdraw(self, amount): # decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
+    def withdraw(self, amount): 
         if self.balance < amount:
             print("Insufficient Funds: charging a $5 fee")
             self.balance = self.balance - 5
         else:
             self.balance = self.balance - amount
         return self
-    def display_account_info(self): # print to the console: eg. "Balance: $100"
-        print(f"First name: {self.first_name}\nLast name: {self.last_name}\nBalance: ${self.balance}")
-    def yield_interest(self): # increases the account balance by the current balance * the interest rate (as long as the balance is positive)
+    def display_account_info(self): 
+        print(f"account name: {self.account_name}\nbalance: ${self.balance}\ninterest rate: {self.int_rate}")
+    def yield_interest(self): 
         if self.balance < 0:
             print("no interest rate")
         else:
@@ -28,28 +28,26 @@ class User:
         self.name = name
         self.age = age
         self.dict_of_accounts = {}
-    @classmethod
-    def display_all_accounts(cls):
-        print(cls.list_of_accounts)
     def create_account(self, account):
-        self.dict_of_accounts[account.name] = account
+        self.dict_of_accounts[account.account_name] = f"balance - ${account.balance}, interest rate - {account.int_rate}"
     def make_deposit(self, account, amount):
-        self.dict_of_accounts[account.name].deposit(amount)
+        self.dict_of_accounts[account.account_name].deposit(amount)
         return self
     def make_withdrawal(self, amount):
         self.account.withdraw(amount)
         return self
     def display_user_balance(self):
         print(f"Balance: ${self.account.balance}")
+    def display_all_accounts(self):
+        print(f"{self.dict_of_accounts}")
 
 autumn_han = User("Autumn", 28)
+account1 = BankAccount("account1", 300, 0.02)
+account2 = BankAccount("account2", 500, 0.04)
+autumn_han.create_account(account1)
+autumn_han.create_account(account2)
+autumn_han.display_all_accounts()
 
-user = User()
-account = BankAccount('name', 0.1, 100)
-user.addAcc(account)
-acc = user.dict_of_accounts['name']
-user.make_withdrawal(acc, 100)
-account = None
 
-user.make_withdrawal('name', 100)
+
 
