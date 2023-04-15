4e985276b8cdbe3e0192dbf52d2d7f7e3ff5b263
Autumn Han <autumn9894@gmail.com>
2023-04-15 00:03:11 -0500

    dojo_pets assignment

---
+++ modularizing/dojo_pets.py
@@ -0,0 +1,29 @@
+class Ninja:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+    def walk(Pet):
+        pass
+    def feed(self):
+        pass
+    def bathe(self):
+        pass
+
+class Pet:
+    def __init__(self, name, type, tricks, health, energy):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+    def sleep(self):
+        pass
+    def eat(self):
+        pass
+    def play(self):
+        pass
+    def noise(self):
+        pass
